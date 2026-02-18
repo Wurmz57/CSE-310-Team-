66273d39ee0d570fd4dfcf1d8e1de8de8525e9ad
Wurmz57 <jaden.mons@hotmail.com>
2026-02-18 14:59:31 -0700

    Fix map changes
    Adds boxes and sword to right location

--- scripts/player.gd
+++ scripts/player.gd
@@ -33,6 +33,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
+		velocity += get_gravity() * delta
 		coyote_time += delta
 	else:
 		coyote_time = 0
