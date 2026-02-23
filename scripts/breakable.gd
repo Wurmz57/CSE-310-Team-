55407d4a80c81b3958835d162c2809e443844b8f
Thomas Meyers <funclemoz@gmail.com>
2026-02-23 13:23:54 -0700

    Breaking animations done
    It took a bit, but I think I was able to figure it out. I really hope that something doesn't break.

--- scripts/breakable.gd
+++ scripts/breakable.gd
@@ -3,12 +3,15 @@ extends StaticBody2D
 @onready var solid_collision = $SolidCollision
 @onready var hurtbox = $Hurtbox
 @onready var hurtbox_collision = $Hurtbox/HurtboxCollision
-@onready var sprite = $Sprite2D
+@onready var ani_sprite = $AnimatedSprite2D
 var broken := false
 var hp: int
 @export var max_hp = 1
 @export var break_on_any_hit = true
 @export var can_break = true
+@export var break_animation_name: String = "break"
+@export var idle_name: String = "unbroken"
+@export var broken_name: String = "broken"
 #@export var damage = 1
 		
 
@@ -20,11 +23,14 @@ func _break() -> void:
 	if broken:
 		return
 	broken = true	
+	print("Playing break animation:", break_animation_name)
 	solid_collision.set_deferred("disabled", true)
 	hurtbox_collision.set_deferred("disabled", true)
 
-	sprite.visible = false
-	
+	ani_sprite.stop()
+	ani_sprite.play(break_animation_name)
+	await ani_sprite.animation_finished
+	ani_sprite.play(broken_name)
 
 func _on_hurtbox_area_entered(area: Area2D) -> void:
 	if not area.is_in_group("hitbox"):
