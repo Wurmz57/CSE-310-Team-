496ec474e6387a3102cdad4cf529dfde13ff85d0
Hunhay2516 <hunhay2516@gmail.com>
2026-02-23 15:18:45 -0700

    "walk into door with key collected" now works

--- scripts/door.gd
+++ scripts/door.gd
@@ -7,17 +7,24 @@ extends StaticBody2D
 @export var open_id: StringName = &"key"
 @export var HP_lock := false
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if HP_lock == false:
-		if PlayerProgress.has_ability(open_id):
-			solid_collision.set_deferred("disabled", true)
-			sprite.visible = false
-			sprite2.visible = false
-	else:
-		pass
+#func _process(delta: float) -> void:
+	#if HP_lock == false:
+		#if PlayerProgress.has_ability(open_id):
+			#solid_collision.set_deferred("disabled", true)
+			#sprite.visible = false
+			#sprite2.visible = false
+	#else:
+		#pass
 #if PlayerProgress.has_ability(&"sword"):
+
+
+
+func _on_unlock_door_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("player"):
+		return
+	if PlayerProgress.has_ability(open_id):
+		solid_collision.set_deferred("disabled", true)
+		sprite.visible = false
+		sprite2.visible = false
