672dc7bfdd357409a0b4220a0d2e734ee0322f3d
LazyBones1313 <sha21018@byui.edu>
2026-03-23 14:07:46 -0600

    Deleted all non sprite files from sprite archive

--- scenes/dust_particles.gd
+++
@@ -1,6 +0,0 @@
-extends GPUParticles2D
-
-func _ready():
-	restart()
-	await finished
-	queue_free()
--- scripts/DoorEvents.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-@warning_ignore("unused_signal")
-
-signal enemy_defeated(door_id: StringName)
